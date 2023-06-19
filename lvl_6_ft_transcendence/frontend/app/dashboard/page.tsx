eadf3c4d7285653671f498bae04bb1cb00b0b87a
Rodrigo <ramos03rodrigo@gmail.com>
2023-06-19 17:58:31 +0100

    upd: dashboard design

--- lvl_6_ft_transcendence/frontend/app/dashboard/page.tsx
+++ lvl_6_ft_transcendence/frontend/app/dashboard/page.tsx
@@ -1,52 +1,52 @@
 import Image from 'next/image'
-import { TbBrandAppleArcade } from 'react-icons/tb'
+import Link from 'next/link'
 
-type Props = {
-	children: JSX.Element[]
-}
+import FriendsList from './friendsList'
 
-
-function Card({ children }: Props) {
+function Card({ children, path }: { children: JSX.Element[], path: string }) {
 	return (
-		<div className='group h-full'>
-			<div className='bg-blue-600 h-full w-[17vw] rounded text-sm drop-shadow-xl shadow-blue-600 group-hover:animation-delay-200 transition-all duration-200 group-hover:-translate-y-8 group-hover:animate-cardBounce items-center flex flex-col text-center space-y-6 place-content-center'>
+		<Link href={path} className="mx-auto bg-black/10 w-1/3 relative group border-2 border-white/20 hover:border-white rounded-xl">
+			<div className="absolute -inset-1 bg-gradient-to-r from-[#FB37FF] to-[#F32E7C] rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 m-1"></div>
+			<div className="flex flex-col relative transition-all duration-700 opacity-50 group-hover:opacity-100 text-white rounded-lg text-center place-content-center align-middle">
 				{children}
 			</div>
-		</div>
+		</Link>
 	)
 }
 
 export default function dashboard() {
 	return (
-		<div className='flex flex-col space-y-12'>
-			<div>
-				<Image
-					src={'/logo.png'}
-					alt='logo picture'
-					width={135}
-					height={125}
-				/>
+		<div className='flex'>
+			<Image
+				src={'/logo.png'}
+				alt='logo picture'
+				width="0"
+				height="0"
+				sizes="100vw"
+				className="fixed w-40 h-28"
+			/>
 
+			<div className='flex flex-col w-3/4 place-content-center gap-y-8'>
+				<div className='flex mx-auto space-x-12 h-96 px-24'>
+					<Card path='/game'>
+						<div className='text-2xl'>Play</div>
+						<div className=''>play against someone </div>
+					</Card>
+					<Card path='/game'>
+						<div className='text-md'>Leaderboard</div>
+						<div></div>
+					</Card>
+					<Card path='/game'>
+						<div className='text-2xl'>Profile</div>
+						<div>lorem ipsum dolor sit </div>
+					</Card>
+				</div>
 			</div>
 
-			<div className='flex mx-auto space-x-12 h-[50vh]'>
-
-				<Card>
-					<div className='text-2xl'>Play</div>
-					<TbBrandAppleArcade size={124} color='white' />
-					<div>lorem ipsum dolor sit</div>
-				</Card>
-
-				<Card>
-					<div className='text-2xl'>Play</div>
-					<TbBrandAppleArcade size={124} color='white' />
-				</Card>
-
-				<Card>
-					<TbBrandAppleArcade size={148} color='white' />
-					<div className='text-2xl'>Play</div>
-				</Card>
+			<div className='flex h-screen w-1/4'>
+				<FriendsList />
 			</div>
-		</div>
+
+		</div >
 	)
 }
--- lvl_6_ft_transcendence/frontend/tailwind.config.js
+++ lvl_6_ft_transcendence/frontend/tailwind.config.js
@@ -22,8 +22,5 @@ module.exports = {
 				},
 			},
 		},
-	},
-	plugins: [
-		require("tailwindcss-animation-delay"),
-	],
+	}
 }
