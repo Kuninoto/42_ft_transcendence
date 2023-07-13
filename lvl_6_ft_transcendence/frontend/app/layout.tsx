ecfd42e724f8ee2be2605847147062cd18199416
ramos21rodrigo <ramos03rodrigo@gmail.com>
2023-07-13 17:02:15 +0100

    some fixes

--- lvl_6_ft_transcendence/frontend/app/layout.tsx
+++ lvl_6_ft_transcendence/frontend/app/layout.tsx
@@ -1,15 +1,45 @@
 'use client'
 
-import { AuthProvider } from '@/contexts/AuthContext'
+import { AuthProvider, useAuth } from '@/contexts/AuthContext'
 import { ChatProvider } from '@/contexts/ChatContext'
 import { Press_Start_2P } from 'next/font/google'
 import { FullScreen, useFullScreenHandle } from 'react-full-screen'
+import type { GetServerSideProps } from 'next'
 import { AiOutlineFullscreen, AiOutlineFullscreenExit } from 'react-icons/ai'
+import { MdOutlineExitToApp } from 'react-icons/md'
+import { usePathname } from 'next/navigation'
 
 import Chat from './chat/page'
 import './globals.css'
 
 const pressStart = Press_Start_2P({ subsets: ['latin'], weight: '400' })
+
+function FixedPanel({ handle }: { handle: any} ){
+
+	const path = usePathname()
+
+	const { logout } = useAuth()
+
+	return (
+		<div className="fixed bottom-4 left-4 flex items-center rounded border-2 border-white p-2">
+			{!handle.active ? (
+				<button onClick={handle.enter}>
+					<AiOutlineFullscreen size={48} />
+				</button>
+			) : (
+				<button onClick={handle.exit}>
+					<AiOutlineFullscreenExit size={48} />
+				</button>
+			)}
+
+			{ path !== '/' && path !== '/game' &&
+				<button onClick={logout}>
+					<MdOutlineExitToApp size={48}/>
+				</button> }
+
+		</div>
+	)
+}
 
 export default function RootLayout({
 	children,
@@ -29,17 +59,7 @@ export default function RootLayout({
 								<Chat />
 							</div>
 
-							<div className="fixed bottom-4 left-4 flex items-center rounded border-2 border-white p-2">
-								{!handle.active ? (
-									<button onClick={handle.enter}>
-										<AiOutlineFullscreen size={48} />
-									</button>
-								) : (
-									<button onClick={handle.exit}>
-										<AiOutlineFullscreenExit size={48} />
-									</button>
-								)}
-							</div>
+							<FixedPanel handle={handle}/>
 						</FullScreen>
 					</body>
 				</html>
