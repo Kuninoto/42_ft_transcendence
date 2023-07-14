970edd61e0a0fad53c718488e131b0cc7b40a88a
Nuno Miguel Nuno Carvalho <nnuno-ca@c1r12s11.42porto.com>
2023-07-14 17:22:47 +0100

    friendships/friend requests fully working

--- lvl_6_ft_transcendence/backend/src/typeorm/index.ts
+++ lvl_6_ft_transcendence/backend/src/typeorm/index.ts
@@ -1,8 +1,7 @@
 import { Friendship } from 'src/entity/friendship.entity';
 import { User } from '../entity/user.entity';
-import { FriendRequest } from 'src/entity/friend-request.entity';
 
-const entities = [User, FriendRequest, Friendship];
+const entities = [User, Friendship];
 
-export { User, FriendRequest, Friendship };
+export { User, Friendship };
 export default entities;
