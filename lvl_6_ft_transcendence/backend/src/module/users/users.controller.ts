61ec5e654f572dcd253a4c435373bf1b87e7e99a
Nuno Miguel Nuno Carvalho <nnuno-ca@c1r15s18.42porto.com>
2023-07-26 16:36:30 +0100

    update imports

--- lvl_6_ft_transcendence/backend/src/module/users/users.controller.ts
+++ lvl_6_ft_transcendence/backend/src/module/users/users.controller.ts
@@ -8,13 +8,13 @@ import {
   Req,
 } from '@nestjs/common';
 import { ApiNotFoundResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { NonNegativeIntPipe } from 'src/common/pipe/non-negative-int.pipe';
 import { UsersService } from './users.service';
 import { JwtAuthGuard } from 'src/module/auth/guard/jwt-auth.guard';
-import { ErrorResponse } from '../common/types/types/error-response.interface';
-import { UserProfile } from '../../../../common/types/user-profile.interface';
-import { NonNegativeIntPipe } from 'src/common/pipe/non-negative-int.pipe';
-import { UserSearchInfo } from '../../../../common/types/user-search-info.interface';
+import { ErrorResponse } from '../../../../common/types/error-response.interface';
 import { User } from 'src/typeorm';
+import { UserProfile } from '../../../../common/types/user-profile.interface';
+import { UserSearchInfo } from '../../../../common/types/user-search-info.interface';
 
 @ApiTags('users')
 @UseGuards(JwtAuthGuard)
