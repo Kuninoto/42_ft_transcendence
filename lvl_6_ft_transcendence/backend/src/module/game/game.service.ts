015b247b4b263f83d4ef17caad22799c72dbebac
Kuninoto <nunorebelocarvalho@sapo.pt>
2023-07-29 00:51:21 +0100

    upd: steps into game queue

--- lvl_6_ft_transcendence/backend/src/module/game/game.service.ts
+++ lvl_6_ft_transcendence/backend/src/module/game/game.service.ts
@@ -1,33 +1,71 @@
 import { Injectable } from '@nestjs/common';
-import { ClientIdToUserIdMap } from './ClientIdToUserId';
+import { ClientToUserInfoMap } from './ClientToUserInfoMap';
 import { GameQueue } from './GameQueue';
 import { UsersService } from '../users/users.service';
 import { UserStatus } from 'src/common/types/user-status.enum';
+import { Server, Socket } from 'socket.io';
+import {
+  GameQueueDataDTO,
+  PlayerSide,
+  QueueMessage,
+} from './dto/game-queue-data.interface';
 
 @Injectable()
 export class GameService {
   constructor(
     private gameQueue: GameQueue,
-    private clientIdToUserIdMap: ClientIdToUserIdMap,
+    private clientToUserInfoMap: ClientToUserInfoMap,
     private usersService: UsersService,
   ) {}
 
-  public queueToLadder(newClientId: string, newUserId: number): void {
-    if (this.clientIdToUserIdMap.isUserIdAlreadyRegistered(newUserId)) {
+  public registerNewClientInfo(newClient: Socket, newClientUID: number): void {
+    if (this.clientToUserInfoMap.isUserIdAlreadyRegistered(newClientUID)) {
       throw new Error('Client is already connected');
     }
+    this.clientToUserInfoMap.registerNewClientInfo(newClient, newClientUID);
+  }
 
-    this.clientIdToUserIdMap.addPair(newClientId, newUserId);
-    this.gameQueue.enqueue(newClientId);
+  public queueToLadder(
+    playerRooms: Socket[],
+    server: Server,
+    newClient: Socket,
+    newClientUId: number,
+  ): void {
+    this.gameQueue.enqueue(newClient.id);
+    this.usersService.updateUserStatusByUID(newClientUId, UserStatus.IN_QUEUE);
 
-    this.usersService.updateUserStatusByUID(newUserId, UserStatus.IN_QUEUE);
+    // If there's no other player waiting, keep him waiting
+    if (this.gameQueue.size() === 1) {
+      const queueData: GameQueueDataDTO = {
+        side: PlayerSide.LEFT,
+        message: QueueMessage.WAITING_FOR_OPPONENT,
+      };
+
+      newClient.emit('waiting-for-opponent', queueData);
+    } else {
+      const queueData: GameQueueDataDTO = {
+        side: PlayerSide.RIGHT,
+        message: QueueMessage.OPPONENT_FOUND,
+      };
+
+      const playerOneClientId: string = this.gameQueue.dequeue();
+      const playerTwoClientId: string = this.gameQueue.dequeue();
+
+      const playerOne: Socket =
+        this.clientToUserInfoMap.getClientFromClientID(playerOneClientId);
+      const playerTwo: Socket =
+        this.clientToUserInfoMap.getClientFromClientID(playerTwoClientId);
+
+      this.joinPlayersToRoom(server, playerOne, playerTwo);
+    }
 
     console.log('gameQueue now has ' + this.gameQueue.size() + ' elements!');
   }
 
   public leaveLadderQueue(clientID: string): void {
     this.gameQueue.removePlayerFromQueue(clientID);
-    const userId: number | undefined = this.clientIdToUserIdMap.removePlayerFromMap(clientID);
+    const userId: number | undefined =
+      this.clientToUserInfoMap.removePlayerFromMap(clientID);
 
     if (userId)
       this.usersService.updateUserStatusByUID(userId, UserStatus.ONLINE);
@@ -35,30 +73,26 @@ export class GameService {
     console.log('gameQueue now has ' + this.gameQueue.size() + ' elements!');
   }
 
-  public dequeueTwoPlayers(): { player1ID: string; player2ID: string } {
-    const player1ID: string = this.gameQueue.dequeue();
-    const player2ID: string = this.gameQueue.dequeue();
+  private joinPlayersToRoom(
+    server: Server,
+    playerOne: Socket,
+    playerTwo: Socket,
+  ): void {
+    const roomName: string = playerOne.id + ' vs. ' + playerTwo.id;
 
-    console.log('queue size = ' + this.gameQueue.size());
+    // Join both players to the same room
+    playerOne.join(roomName);
+    playerTwo.join(roomName);
 
-    return { player1ID, player2ID };
-  }
+    // Assign opponentUID's respectively
+    playerOne.data.opponentUID = this.clientToUserInfoMap.getUserIdFromClientId(
+      playerTwo.id,
+    );
+    playerTwo.data.opponentUID = this.clientToUserInfoMap.getUserIdFromClientId(
+      playerOne.id,
+    );
 
-  public areTwoPlayersWaiting(): boolean {
-    return this.gameQueue.size() >= 2;
-  }
-
-  public addClientIdUserIdPair(newClientId: string, newUserId: number): void {
-    if (this.clientIdToUserIdMap.isUserIdAlreadyRegistered(newUserId)) {
-      throw new Error();
-    }
-    console.log('newClientId = ' + newClientId);
-    console.log('newUserId = ' + newUserId);
-
-    this.clientIdToUserIdMap.addPair(newClientId, newUserId);
-  }
-
-  public getUserIdFromClientId(clientId: string): number {
-    return this.clientIdToUserIdMap.getUserIdFromClientId(clientId);
+    // emit to both players their respective opponent id
+    // server.emit() ?
   }
 }
--- lvl_6_ft_transcendence/backend/src/typeorm/index.ts
+++ lvl_6_ft_transcendence/backend/src/typeorm/index.ts
@@ -1,6 +1,6 @@
 import { BlockedUser } from 'src/entity/blocked-user.entity';
 import { Friendship } from 'src/entity/friendship.entity';
-import { GameInfo } from 'src/entity/game-info.entity';
+import { GamesInfo } from 'src/entity/game-info.entity';
 import { MatchHistory } from 'src/entity/match-history.entity';
 import { UserRecord } from 'src/entity/user-record.entity';
 import { User } from '../entity/user.entity';
@@ -8,11 +8,11 @@ import { User } from '../entity/user.entity';
 const entities = [
   BlockedUser,
   Friendship,
-  GameInfo,
+  GamesInfo,
   MatchHistory,
   UserRecord,
   User,
 ];
 
-export { BlockedUser, Friendship, GameInfo, MatchHistory, UserRecord, User };
+export { BlockedUser, Friendship, GamesInfo, MatchHistory, UserRecord, User };
 export default entities;
