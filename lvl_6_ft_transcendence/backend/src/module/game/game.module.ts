c1b4f40c846933192081fe380c31a64d81eaacfd
Kuninoto <nunorebelocarvalho@sapo.pt>
2023-07-30 14:47:54 +0100

    upd: implement #19

--- lvl_6_ft_transcendence/backend/src/module/game/game.module.ts
+++ lvl_6_ft_transcendence/backend/src/module/game/game.module.ts
@@ -3,13 +3,13 @@ import { AuthModule } from '../auth/auth.module';
 import { UsersModule } from '../users/users.module';
 import { GameController } from './game.controller';
 import { GameQueue } from './GameQueue';
-import { ClientToUserInfoMap } from './ClientToUserInfoMap';
+import { ClientIdToClientInfoMap } from './ClientIdToClientInfoMap';
 import { GameService } from './game.service';
 import { GameGateway } from './game.gateway';
 
 @Module({
   imports: [AuthModule, UsersModule],
   controllers: [GameController],
-  providers: [GameQueue, ClientToUserInfoMap, GameService, GameGateway],
+  providers: [GameQueue, ClientIdToClientInfoMap, GameService, GameGateway],
 })
 export class GameModule {}
