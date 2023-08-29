adfb9883dc195e7821f8487e7a85b34706c32b92
Nuno Miguel Nuno Carvalho <nnuno-ca@c1r13s9.42porto.com>
2023-08-29 17:08:12 +0100

    upd: ball colors on /themes

--- lvl_6_ft_transcendence/frontend/common/themes.ts
+++ lvl_6_ft_transcendence/frontend/common/themes.ts
@@ -2,47 +2,53 @@ interface IThemes {
 	[name: string]: {
 		background: string
 		paddle: string
-		// ball: string
+		ball: string
 	}
 }
+
+const HEX_WHITE: string = '#FFFFFF';
+const HEX_BLACK: string = '#000000';
 
 export const themes: IThemes = {
 	default: {
 		background: 'default.png',
 		paddle: 'white.png',
+		ball: HEX_WHITE,
 	},
 	fortyTwo: {
 		background: 'forty_two.jpg',
 		paddle: 'black.jpeg',
+		ball: HEX_BLACK,
 	},
 	candyland: {
 		background: 'candyland.jpg',
 		paddle: 'black.jpeg',
+		ball: HEX_BLACK,
 	},
 	halloween: {
 		background: 'halloween.jpg',
 		paddle: 'white.jpeg',
+		ball: HEX_WHITE,
 	},
 	neon: {
 		background: 'neon_city.jpg',
 		paddle: 'white.jpeg',
+		ball: HEX_WHITE,
 	},
 	underwater: {
 		background: 'underwater.jpg',
 		paddle: 'white.jpeg',
+		ball: HEX_WHITE,
 	},
 	space: {
 		background: 'space.jpg',
 		paddle: 'white.jpeg',
+		ball: HEX_WHITE,
 	},
 	//safari: {
 	//	background: 'safari.png',
 	//	paddle: '',
 	//},
-	// mikao: {
-	// 	background: 'mikao.jpeg',
-	// 	paddle: 'black.jpeg',
-	// },
 	// miki: {
 	// 	background: 'miki.png',
 	// 	paddle: 'white.png',
