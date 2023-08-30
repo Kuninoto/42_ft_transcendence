cac885990685a85a2aa0bcaada566bc69a909de8
Nuno Miguel Nuno Carvalho <nnuno-ca@c1r13s9.42porto.com>
2023-08-30 17:32:54 +0100

    upd: fix #108

--- lvl_6_ft_transcendence/backend/src/main.ts
+++ lvl_6_ft_transcendence/backend/src/main.ts
@@ -118,7 +118,7 @@ async function bootstrap(): Promise<void> {
   app.useGlobalFilters(new Passport42ExceptionFilter());
   app.setGlobalPrefix('api');
 
-  await app.listen(3000, () => logger.log('Listening on port 3000'));
+  await app.listen(3000, () => logger.log('Listening at http://localhost:3000/api'));
 }
 
 bootstrap();
