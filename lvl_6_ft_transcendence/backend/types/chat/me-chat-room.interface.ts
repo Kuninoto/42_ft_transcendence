061ddc65e2719f5dafc540da9f4a8d2199860e75
Nuno Miguel Nuno Carvalho <nnuno-ca@c1r13s11.42porto.com>
2023-08-31 15:54:18 +0100

    upd: clean

--- lvl_6_ft_transcendence/backend/types/chat/me-chat-room.interface.ts
+++ lvl_6_ft_transcendence/backend/types/chat/me-chat-room.interface.ts
@@ -1,10 +1,8 @@
-import { ChatRoomRoles } from './chat-room-roles.enum';
 import { Chatter } from './chatter.interface';
 
 export interface MeChatRoom {
   id: number;
   name: string;
   ownerName: string;
-  myRole: ChatRoomRoles;
   participants: Chatter[];
 }
