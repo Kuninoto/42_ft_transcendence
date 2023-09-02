30b3f1bb75af56d94c19db19e29ae543180ab74b
Nuno Miguel Nuno Carvalho <nnuno-ca@c1r14s12.42porto.com>
2023-09-02 16:00:30 +0100

    upd: fix

--- lvl_6_ft_transcendence/manage_hosts.sh
+++ lvl_6_ft_transcendence/manage_hosts.sh
@@ -3,21 +3,16 @@
 # if delete option is passed, erase all ft_transcendence
 # entries from hosts
 if [[ $1 == "-d" ]] || [[ $1 == "--delete" ]]; then
-    sed -i "/ft_transcendence/d" /etc/hosts
     sed -i "/pongfight/d" /etc/hosts
     exit 0
 fi
 
-NGINX_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' nginx)
+NEST_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' nest)
 
 # Removing previous entries
-sed -i "/ft_transcendence/d" /etc/hosts
 sed -i "/pongfight/d" /etc/hosts
 
 # Write the new entries to /etc/hosts
-sed -i "1i'$NGINX_IP' ft_transcendence" /etc/hosts
-sed -i "1i'$NGINX_IP' ft_transcendence.com" /etc/hosts
-sed -i "1i'$NGINX_IP' www.ft_transcendence.com" /etc/hosts
 sed -i "1i'$NGINX_IP' pongfight" /etc/hosts
 sed -i "1i'$NGINX_IP' pongfight.com" /etc/hosts
 sed -i "1i'$NGINX_IP' www.pongfight.com" /etc/hosts
