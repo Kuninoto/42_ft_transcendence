001f295d2078a92dc28d390a03479a10e06185e4
Nuno Miguel Nuno Carvalho <nnuno-ca@c1r14s11.42porto.com>
2023-09-07 19:53:44 +0100

    upd: types

--- lvl_6_ft_transcendence/frontend/common/types/chat/request/mute-user-request.interface.ts
+++ lvl_6_ft_transcendence/frontend/common/types/chat/request/mute-user-request.interface.ts
@@ -1,7 +1,6 @@
 import { MuteDuration } from "../mute-duration.enum";
 
 export interface MuteUserRequest {
-  readonly roomId: number;
   readonly userId: number;
   readonly duration: MuteDuration;
 }
